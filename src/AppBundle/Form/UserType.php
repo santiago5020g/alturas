1c932cedd8322e61e779585139b33ea27260d2f2
Santiago <santiago5020g@hotmail.com>
2017-02-11 19:56:38 -0500

    Se Agregan estilos al formulario login y se crea el combo rol entity type en el formulario usuarios

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('email', EmailType::class, array('attr' => array('class' => 'form-control'),'label'=>'E-mail'))
+        ->add('password', PasswordType::class, array('attr' => array('class' => 'form-control'),'label'=>'Clave'))
+        ->add('isActive',CheckboxType::class,array('label'=>'Activo'))
+        ->add('roll', EntityType::class, array(
+        'attr' => array('class' => 'form-control'),
+        // query choices from this entity
+        'class' => 'AppBundle:Roll',
+
+        'placeholder' => 'Seleccione el rol de usuario...',
+        // use the User.username property as the visible option string
+        'choice_label' => 'description',
+        ));
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
